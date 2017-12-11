089a6b2268ddcf62c5a19a6a6c5bc4e415ffeaf2
Ashley Johnson <ashvalejohn@gmail.com>
2017-12-11 13:31:28 -0800

    Style login form

--- frontend/components/session/login.jsx
+++ frontend/components/session/login.jsx
@@ -3,8 +3,17 @@ import React, { Component } from 'react';
 class Login extends Component {
   render(){
     return (
-      <div>
+      <div className='auth'>
         <h1>Log in</h1>
+        <form action="" className='auth__form'>
+        <label htmlFor="" className='auth__form-field'>Email Address:
+          <input type="email"/>
+        </label>
+        <label htmlFor="" className='auth__form-field'>Password:
+          <input type="password"/>
+        </label>
+        </form>
+        <button className='auth__submit'>Log In</button>
       </div>
     )
   }
