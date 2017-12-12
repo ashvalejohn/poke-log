84cb30437bab6ef50547269bb362d3a6c016fea1
Ashley Johnson <ashvalejohn@gmail.com>
2017-12-12 12:00:35 -0800

    Create settings component, add proteted route to app

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,9 +3,11 @@ import { Route } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route-util';
 import LoginContainer from './session/login-container';
 import SignupContainer from './session/signup-container';
+import Settings from './session/signup-container';
 
 const App = () => (
   <div>
+    <ProtectedRoute path='/settings' component={Settings} />
     <AuthRoute path='/login' component={LoginContainer} />
     <AuthRoute path='/signup' component={SignupContainer} />
   </div>
