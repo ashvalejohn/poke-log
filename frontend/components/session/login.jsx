f3aad9c5eabc6666e94175b30df7bb0ee66bfb0a
Ashley Johnson <ashvalejohn@gmail.com>
2017-12-11 17:13:33 -0800

    Style login page

--- frontend/components/session/login.jsx
+++ frontend/components/session/login.jsx
@@ -4,14 +4,14 @@ class Login extends Component {
   render(){
     return (
       <div className='auth'>
-        <h1>Log in</h1>
+        <h1 className='auth__title'>Log in</h1>
         <form action="" className='auth__form'>
-        <label htmlFor="" className='auth__form-field'>Email Address:
-          <input type="email"/>
-        </label>
-        <label htmlFor="" className='auth__form-field'>Password:
-          <input type="password"/>
-        </label>
+          <label htmlFor="" className='auth__form-field'>Email Address
+            <input type="email"/>
+          </label>
+          <label htmlFor="" className='auth__form-field'>Password
+            <input type="password"/>
+          </label>
         </form>
         <button className='auth__submit'>Log In</button>
       </div>
