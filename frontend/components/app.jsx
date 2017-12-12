0beaafd9073e5b93506ca427d5d04a6bbc07671a
Nicholas <nwilliams770@gmail.com>
2017-12-12 09:35:58 -0800

    add log poke container

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import LoginContainer from './session/login-container';
 import SignupContainer from './session/signup-container';
+import LogPokeContainer from './log-poke/log-poke-container';
 
 const App = () => (
   <div>
     <Route path='/login' component={LoginContainer} />
     <Route path='/signup' component={SignupContainer} />
+    <Route path='/logpoke' component={LogPokeContainer} />
   </div>
 )
 
