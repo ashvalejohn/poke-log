682c3fc10ec3b547f033ee3e256751f94023e6ee
Nicholas <nwilliams770@gmail.com>
2017-12-12 09:40:09 -0800

    add log poke component

---
+++ frontend/components/log-poke/log-poke-container.jsx
@@ -0,0 +1,7 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+import LogPoke from './log-poke';
+
+export default withRouter(null, null)(LogPoke);
+
---
+++ frontend/components/log-poke/log-poke.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class LogPoke extends React.Component {
+    render() {
+        return(
+            <h1> knick-knack patty-whack we're connected!</h1>
+        )
+    }
+}
