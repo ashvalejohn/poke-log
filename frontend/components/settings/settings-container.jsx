6e4d24849233af498897efc531cbdb6debee6a97
Nicholas <nwilliams770@gmail.com>
2017-12-13 18:01:31 -0800

    finish redux cycle

--- frontend/components/settings/settings-container.jsx
+++ frontend/components/settings/settings-container.jsx
@@ -1,14 +1,16 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import Settings from './settings';
-// import { logout } from session actions
+import { logout } from '../../actions/session-actions';
 
 const mapStateToProps = state => ({
-  // 
+  // dosage: state.currentUser.dosage
+  // name: state.currentUser.name
 });
 
-const mapDispatchToProps = dispatch => ({
-  // ADD logout
+const mapDispatchToProps = (dispatch) => ({
+  logout: () => dispatch(logout())
+  
 });
 
 export default withRouter(connect(null, null)(Settings));
