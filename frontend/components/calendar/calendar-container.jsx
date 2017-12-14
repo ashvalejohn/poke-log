b90873d4f771c680cc16569260bac5cf669ebdd6
Ashley Johnson <ashvalejohn@gmail.com>
2017-12-13 18:07:02 -0800

    Create calendar component

---
+++ frontend/components/calendar/calendar-container.jsx
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import Calendar from './calendar';
+
+// const mapStateToProps = state => (
+
+// );
+
+// const mapDispatchToProps = dispatch => (
+
+// );
+
+export default withRouter(connect(null, null)(Calendar))
---
+++ frontend/components/calendar/calendar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Calendar = () => (
+  <div className='calendar'>
+    <h1 className='calendar__title'>Calendar</h1>
+    <select name="" id="">
+      <option value="">December</option>
+    </select>
+    <div className='calendar__log'>
+      
+    </div>
+  </div>
+)
+
+
+export default Calendar;
