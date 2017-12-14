f08c44e66b63c0d6209def4e385933088a4c2be5
Nicholas <nwilliams770@gmail.com>
2017-12-13 19:46:24 -0800

    add mapStateToProps

--- frontend/components/settings/settings-container.jsx
+++ frontend/components/settings/settings-container.jsx
@@ -1,11 +1,12 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import Settings from './settings';
+
 import { logout } from '../../actions/session-actions';
 
 const mapStateToProps = state => ({
-  // dosage: state.currentUser.dosage
-  // name: state.currentUser.name
+  dosage: state.session.currentUser.dosage,
+  name: state.session.currentUser.name
 });
 
 const mapDispatchToProps = (dispatch) => ({
