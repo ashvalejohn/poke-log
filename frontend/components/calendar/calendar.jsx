408dde9826b47990b0bdfbb2012ca61d5ae3676f
Ashley Johnson <ashvalejohn@gmail.com>
2017-12-13 18:50:18 -0800

    Allow user to select month

--- frontend/components/calendar/calendar.jsx
+++ frontend/components/calendar/calendar.jsx
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Calendar = () => (
-  <div className='calendar'>
-    <h1 className='calendar__title'>Calendar</h1>
-    <select name="" id="">
-      <option value="">December</option>
-    </select>
-    <div className='calendar__log'>
-      
-    </div>
-  </div>
-)
+class Calendar extends Component {
+  constructor(props){
+    super(props);
 
+    const date = new Date(), locale = "en-us", month = date.toLocaleString(locale, { month: "long" });
+
+    this.state = {
+      month: month,
+    }
+
+    this.handleChangeMonth = this.handleChangeMonth.bind(this);
+  }
+
+  handleChangeMonth(e){
+    e.preventDefault();
+    this.setState({
+      month: e.target.value,
+    });
+  }
+
+  render(){
+    return (
+      <div className='calendar'>
+        <h1 className='calendar__title'>Calendar</h1>
+        <select defaultValue={this.state.month} onChange={this.handleChangeMonth}>
+          <option value="January">January</option>
+          <option value="February">February</option>
+          <option value="March">March</option>
+          <option value="April">April</option>
+          <option value="May">May</option>
+          <option value="June">June</option>
+          <option value="July">July</option>
+          <option value="August">August</option>
+          <option value="September">September</option>
+          <option value="October">October</option>
+          <option value="November">November</option>
+          <option value="December">December</option>
+        </select>
+        <div className='calendar__log'>
+
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Calendar;
