431f1603d5b0cdafc2ff6385e8a9155efa45fb45
Ashley Johnson <ashvalejohn@gmail.com>
2017-12-14 22:50:29 -0800

    Get all pokes for user from backend, bad format

--- views/pokes.js
+++ views/pokes.js
@@ -1,13 +1,12 @@
 const jbuilder = require('jbuilder');
+const _poke = require('./poke');
 
 module.exports = (pokes) => {
 
   const output = jbuilder.encode(json => {
     json.set('pokes', json => {
       pokes.forEach(poke => {
-        json.set(poke.id, () => {
-          json.extract(poke, 'id', 'date', 'dose', 'bleed', 'double');
-        })
+        json.set(poke.id, _poke(poke));
       });
     });
   });
