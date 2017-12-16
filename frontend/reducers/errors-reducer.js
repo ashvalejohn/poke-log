890e44010ac9547078c83c752191ca09a45fbe05
Matthew Buckner <bucknermr@gmail.com>
2017-12-15 16:34:14 -0800

    Add poke errors reducer Remove pokeReducer (not being used)

--- frontend/reducers/errors-reducer.js
+++ frontend/reducers/errors-reducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import pokeErrorsReducer from './poke-errors-reducer';
+// import sessionErrorsReducer from './session-errors-reducer';
 
 const errorsReducer = combineReducers({
-  poke: pokeErrorsReducer
+  poke: pokeErrorsReducer,
 });
 
 export default errorsReducer;
--- frontend/reducers/poke-errors-reducer.js
+++ frontend/reducers/poke-errors-reducer.js
@@ -1,11 +1,13 @@
-import { RECEIVE_POKE_ERRORS } from '../actions/log-poke-actions';
+import { RECEIVE_POKE_ERRORS, RECEIVE_POKE } from '../actions/log-poke-actions';
 
 const pokeErrorsReducer = (state = [], action) => {
   Object.freeze(state);
-  
+
   switch(action.type) {
     case RECEIVE_POKE_ERRORS:
       return action.errors;
+    case RECEIVE_POKE:
+      return [];
     default:
       return state;
   }
