7ab83354bf1e3ca930a7dce8039e1320b82dc8cb
Nicholas <nwilliams770@gmail.com>
2017-12-16 12:19:00 -0800

    build out heat-map

---
+++ frontend/components/heat-map/heat-map.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HeatMap extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bleeds: ["Left foot", "Right shoulder", "Nose"];
+    }
+  }
+
+  
+}
