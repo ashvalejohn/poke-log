bfa0e3d2bbce1fe59243cbdcf8eea6702ce4c37a
Nicholas <nwilliams770@gmail.com>
2017-12-16 12:00:20 -0800

    add bleed info to props

--- frontend/components/heat-map/heat-map-container.jsx
+++ frontend/components/heat-map/heat-map-container.jsx
@@ -1,9 +1,18 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import HeatMap from './heat-map';
 
 
+ const mapStateToProps = state => ({
+    // would ideally have all bleeds in an array
+    //  bleeds: state.bleeds
+ });
 
 
+const mapDispatchToProps = dispatch => ({
+    // Need to fetch all pokes with bleeds, then just have array of bleeds
+    // fetchPokes: date => dispatch(fetchPokes(date))
+});
 
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(HeatMap));
+export default withRouter(connect(null, null)(HeatMap));
