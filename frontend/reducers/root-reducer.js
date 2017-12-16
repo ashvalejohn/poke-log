2a0ac01b31d530ee6e7cc79d82e5f38848d1e52d
Nicholas <nwilliams770@gmail.com>
2017-12-16 12:03:43 -0800

    add bleedsReducer to rootReducer

--- frontend/reducers/root-reducer.js
+++ frontend/reducers/root-reducer.js
@@ -3,11 +3,13 @@ import sessionReducer from './session-reducer';
 import settingsReducer from './settings-reducer';
 import logReducer from './log-reducer';
 import errorsReducer from './errors-reducer';
+import bleedsReducer from "./bleeds-reducer";
 
 const rootReducer = combineReducers({
   session: sessionReducer,
   settings: settingsReducer,
   loggedPokes: logReducer,
+  bleeds: bleedsReducer,
   errors: errorsReducer
 });
 
