1972b0b889422c30eabb60d03297e53c0da12481
Ashley Johnson <ashvalejohn@gmail.com>
2018-01-09 11:41:09 -0800

    Insert icons

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -6,21 +6,26 @@ const Splash = () => (
     <h2>An app for people with Hemophilia.</h2>
     <main>
       <section className='feature feature--poke'>
-        <img src="" alt=""/>
-        <h3>Log your treatments</h3>
-        <p>Record your infusions (pokes) and injuries (bleeds).</p>
+        <div className='feature__icon'></div>
+        <article>
+          <h3>Log your treatments</h3>
+          <p>Record your infusions (pokes) and injuries (bleeds).</p>
+        </article>
       </section>
       <section className='feature feature--log'>
-        <img src="" alt=""/>
-        <h3>See your log</h3>
-        <p>Review your pokes and bleeds by month.</p>
+        <div className='feature__icon'></div>
+        <article>
+          <h3>See your log</h3>
+          <p>Review your pokes and bleeds by month.</p>
+        </article>
       </section>
       <section className='feature feature--share'>
-        <img src="" alt=""/>
-        <h3>Share your poke log</h3>
-        <p>Share your pokes and bleeds with caregivers and loved ones.
-        </p>
-        <p className='splash-page__coming-soon'>Coming soon!</p>
+        <div className='feature__icon'></div>
+        <article>
+          <h3>Share your poke log</h3>
+          <p>Share your pokes and bleeds with caregivers and loved ones.</p>
+          <p className='splash-page__coming-soon'>Coming soon!</p>
+        </article>
       </section>
     </main>
     <button className='splash-page__try-it'>Try It</button>
