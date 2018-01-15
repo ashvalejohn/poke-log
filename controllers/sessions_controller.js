8071a0f40c4d667d151107b39e9a52f996927264
Matthew Buckner <bucknermr@gmail.com>
2018-01-14 23:38:10 -0800

    Fix bug where user is not being logged out properly Replace call to logout function with session.destroy()

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -2,10 +2,11 @@ const SessionsController = {
   create: ({id, email, name, dosage}, res) => {
     res.json({ id, email, name, dosage });
   },
-  destroy: ({ user, logout }, res) => {
+  destroy: ({ user, logout, session }, res) => {
+    console.log('session.........', session);
     if (user) {
       const { id, email, name, dosage } = user;
-      logout();
+      session.destroy();
       res.json({ id, email, name, dosage });
     } else {
       res.status(422).json(["Can't logout when not logged in"]);
